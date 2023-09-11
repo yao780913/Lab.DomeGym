f5b1e5df1db6f6a5face3be4d3fd4db39b54ebd6
bill.wang <bill.wang@edgentauems.com>
2023-09-11 15:50:45 +0800

    [feat] Implement a Repository

---
+++ DomeGym.Application/Common/Interfaces/ISessionRepository.cs
@@ -0,0 +1,10 @@
+﻿using DomeGym.Domain.SessionAggregate;
+
+namespace DomeGym.Application.Common.Interfaces;
+
+public interface ISessionRepository
+{
+    Task AddSessionAsync (Session session);
+    Task UpdateSessionAsync (Session session);
+    
+}
